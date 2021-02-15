0e513e317161adf609a8d19bb2bdd256d45fa241
marcusbugge <54141610+marcusbugge@users.noreply.github.com>
2021-02-15 19:54:44 +0100

    Initial Commit of files, some previous work.

---
+++ TAEweb/countdown.js
@@ -0,0 +1,25 @@
+function countdown() {
+    var currentDato = new Date();
+    var eventDato = new Date("16 Feb 2021");
+
+    var currentTime = currentDato.getTime();
+    var eventTime = eventDato.getTime();
+
+    var diff = (eventTime - currentTime + 72000000) / 1000;
+
+
+    var s = Math.floor(diff) % 60;
+    var m = Math.floor(diff / 60) % 60;
+    var h = Math.floor(diff / 3600) % 24;
+    var d = Math.floor(diff / 3600 / 24);
+
+
+    document.getElementById("days").innerHTML = d;
+    document.getElementById("hours").innerHTML = h;
+    document.getElementById("minutes").innerHTML = m;
+    document.getElementById("seconds").innerHTML = s;
+
+}
+
+countdown();
+setInterval(countdown, 1000);
